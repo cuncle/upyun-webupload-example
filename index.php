df3a08f861f95b64bed355c0d8ea89815acdf6ab
cuncle <uncle5@outlook.com>
2016-04-07 09:33:18 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>Webuploader</title>
+        <title>Webuploader-Demo</title>
         
 		<link rel="stylesheet" type="text/css" href="css/webuploader.css"/>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
